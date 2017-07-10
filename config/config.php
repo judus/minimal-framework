06d97b9bda09c0433626336a0edf0e6d95955e5e
Julien Duseyau <julien.duseyau@gmail.com>
2017-07-11 01:43:25 +0200

    Unflat config array

--- config/config.php
+++ config/config.php
@@ -1,18 +1,27 @@
 <?php
 
 return [
-    'app.path' => 'app',
-    'app.configFile' => 'config/config.php',
-    'app.bindingsFile' => 'config/bindings.php',
-    'app.providersFile' => 'config/providers.php',
-    'app.routesFile' => 'config/routes.php',
+    'app' => [
+        'path' => 'app',
+        'configFile' => 'config/config.php',
+        'bindingsFile' => 'config/bindings.php',
+        'providersFile' => 'config/providers.php',
+        'routesFile' => 'config/routes.php',
+    ],
 
-    'modules.path' => 'app',
-    'modules.configFile' => 'Config/config.php',
-    'modules.bindingsFile' => 'Config/bindings.php',
-    'modules.providersFile' => 'Config/providers.php',
-    'modules.routesFile' => 'Config/routes.php',
+    'modules' => [
+        'path' => 'app',
+        'configFile' => 'Config/config.php',
+        'bindingsFile' => 'Config/bindings.php',
+        'providersFile' => 'Config/providers.php',
+        'routesFile' => 'Config/routes.php',
+    ],
 
-    'cache.path' => 'storage/cache',
-    'content.path' => 'storage/content'
+    'cache' => [
+        'path' => 'storage/cache'
+    ],
+
+    'content' => [
+        'path' => 'storage/content'
+    ],
 ];
